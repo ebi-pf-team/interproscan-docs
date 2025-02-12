f4c6e7a3054474944673deb454b59c8a2e56a326
Matthias Blum <mat.blum@gmail.com>
2025-02-12 19:08:45 +0000

    Bump year

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'interproscan-docs'
-copyright = '2024, EMBL-EBI'
+copyright = '2025, EMBL-EBI'
 author = 'EMBL-EBI'
 
 # The version info for the project you're documenting, acts as replacement for
